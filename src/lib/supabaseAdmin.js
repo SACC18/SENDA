36b99d78f298efa83fc8cc8edb89dba848ec8e67
SACC18 <santiagocuamacas18@gmail.com>
2026-03-17 20:28:58 -0500

    Remove hardcoded Supabase service key

--- src/lib/supabaseAdmin.js
+++ src/lib/supabaseAdmin.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseServiceKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwaHptdHZkZnVrbGNvY3NoZGptIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODQ4OTM4OCwiZXhwIjoyMDg0MDY1Mzg4fQ.fgDBvot-PyBPpYIJAdXPHXR-cxABJtV1loVQanRa4qw'
+const supabaseServiceKey = 
 
 export const supabaseAdmin = createClient(supabaseUrl, supabaseServiceKey, {
   auth: {
